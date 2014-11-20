3c03617b9f66e71b4bc45adaee8356b603dd1d90
Dave Poole <davethegr8@gmail.com>
2014-11-19 21:47:59 -0800

    add alter scripts

---
+++ database/alter/2.2.0.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `seasons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `archived` datetime DEFAULT NULL,
+  `name` varchar(128) DEFAULT NULL,
+  `status` enum('active','archived') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `seasons_games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `season_id` int(11) DEFAULT NULL,
+  `game_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
